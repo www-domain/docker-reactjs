2c04a83c30537b01afdbcfdceee013077aebee49
Thomas V Varghese <118618681+www-domain@users.noreply.github.com>
2024-12-30 20:04:35 +0530

    Update sonar-scanner.js

--- sonar-scanner.js
+++ sonar-scanner.js
@@ -1,14 +1,11 @@
-const scannerf = require('sonarqube-scanner');
-scannerf({
-  serverUrl: 'http://3.107.178.131:9000/', // Replace with your SonarQube server URL
-  token: 'sqp_c443a340d90eb8777528edacbeeaa472a6dcb7f4', // Replace with your SonarQube authentication token
-  options: {
-    'sonar.projectKey': 'Docker-React',
-    'sonar.projectName': 'Docker React',
-    'sonar.projectVersion': '1.0',
-    'sonar.sources': 'src',
-    'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
-    'sonar.exclusions': 'node_modules/**',
-    'sonar.test.inclusions': '**/*.test.js,**/*.spec.js,**/*.test.jsx,**/*.spec.jsx',
-  },
+const scanner = require('sonarqube-scanner');
+
+scanner({
+    serverUrl: "http://your-sonarqube-server",
+    options: {
+        "sonar.projectKey": "your-project-key",
+        "sonar.sources": "./src",
+        "sonar.login": "your-login-token"
+    }
 }, () => process.exit());
+
