35f02f002ee761e2cb6474445c4af283a0c8c7e5
Thomas V Varghese <118618681+www-domain@users.noreply.github.com>
2024-12-31 15:46:35 +0530

    Delete config-overrides.js

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-module.exports = function override(config, env) {
-    if (env === 'test') {
-        config.transform = {
-            "^.+\\.(js|jsx)$": "babel-jest"
-        };
-    }
-    return config;
-};
